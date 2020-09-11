16afd5f4b36f19b56de2f05e4a5aadf87ccd0b38
Miguel Agudelo <miguelfinalalternativo@hotmail.com>
2020-09-11 12:01:07 -0500

    Sprites, iniciar combate

---
+++ Assets/Scripts/InitCombat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InitCombat : MonoBehaviour
+{
+    [SerializeField]
+    GameObject button;
+
+    public void Init()
+    {
+        button.SetActive(false);
+        Referee.Instance.NextTurn();
+    }
+}
